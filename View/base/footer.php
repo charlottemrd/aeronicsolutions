9314bad5a30619292547f8780ad2c5b30277a21a
charlottemrd <charlotte@milard.fr>
2021-04-26 16:11:31 +0200

    reseaux

---
+++ View/base/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <p> (c) Copyright tous droits réservés Aeronic Solutions Corporation 2021 </p>
+    <div class="confidentialites">
+        <a href="#">Engagement de confidentialité</a>
+        <a href="#">Utilisation de cookies</a>
+        <a href="#">Conditions d'utilisations</a>
+        <a href="#">Mentions légales</a>
+    </div>
+
+</div>
