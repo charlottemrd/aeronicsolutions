dde21d515a77556e7080c92b4dbee597f5ef9c0f
charlottemrd <56175535+charlottemrd@users.noreply.github.com>
2021-04-30 16:24:00 +0200

    Delete View directory

--- View/base/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-    <p> (c) Copyright tous droits réservés Aeronic Solutions Corporation 2021 </p>
-    <div class="confidentialites">
-        <a href="confidentialite.html">Engagement de confidentialité</a>
-        <a href="Cookies.html">Utilisation de cookies</a>
-        <a href="Conditionsutilisations.html">Conditions d'utilisations</a>
-        <a href="#">Mentions légales</a>
-    </div>
-</div>
