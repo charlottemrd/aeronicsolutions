9d473ed9540e23f2077003a48c047dd0cf770742
charlottemrd <charlotte@milard.fr>
2021-04-30 08:49:03 +0200

    supression dossier aeronic solutions en trop

--- aeronicsolutions/View/base/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer bottom">
-    <p> (c) Copyright tous droits réservés Aeronic Solutions Corporation 2021 </p>
-    <div class="confidentialites">
-        <a href="confidentialite.php">Engagement de confidentialité</a>
-        <a href="cookies.php">Utilisation de cookies</a>
-        <a href="conditionsUtilisation.php">Conditions d'utilisations</a>
-        <a href="mentionsLegales.php">Mentions légales</a>
-    </div>
-</div>
