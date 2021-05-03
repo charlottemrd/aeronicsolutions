06a4b99ec88341d17c92b14288dd38dc718f5bab
charlottemrd <charlotte@milard.fr>
2021-05-03 14:32:42 +0200

    ajout presentationTests.php

---
+++ View/presentationTests.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Aeronic solutions </title>
+
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../assets/presentationTest.css" />
+    <link rel="stylesheet" href="../assets/footer.css" />
+</head>
+<body>
+<nav>
+    <div class="logoC">
+        <a href="index.html">
+            <img src="../assets/Ressources/Images/Logo3.png" width="150" alt="logo" class="logo-image"></a>
+    </div>
+    <div class="linkC">
+        <a href="#">Découvrir</a>
+        <a href="faq.html">FAQ</a>
+    </div>
+    <div class="connexionC">
+        <div class="IMGC">
+            <img class="Bcon" href="index.html" src="../assets/Ressources/Images/account.png" width=30 alt="##"/>
+        </div>
+        <div class="menuD">
+            <div class="bt1">
+                <a href="Inscxription.html"><p>Inscription</p></a>
+            </div>
+            <div class="bt2">
+                <a href="connexion.html"><p>Connexion</p></a>
+            </div>
+        </div>
+
+
+    </div>
+
+</nav>
+
+<div class="test">
+    <div class="bloc1">
+        <div class="bloc2">
+            <div class="title_container">
+                <h3>
+                    Mesure de la température corporelle
+                </h3></div>
+            <p>
+                Fratres consulatu egressus confines vastabantur Constantius reges petit Valentiam ter in suo Constantius crebris dum crebris arma petit Gallorum egressus arma Arelate in limitibus Valentiam in in ter fratres suo Arelate tepore Gallorum perferret reserato Caesaris quorum fratres caeli ter.
+
+
+            </p>
+            <img src="" alt="">
+        </div>
+        <div class="bloc2">
+            <div  class="title_container">
+                <h3>
+                    Test auditif
+                </h3></div>
+            <p>
+                Fratres consulatu egressus confines vastabantur Constantius reges petit Valentiam ter in suo Constantius crebris dum crebris arma petit Gallorum egressus arma Arelate in limitibus Valentiam in in ter fratres suo Arelate tepore Gallorum perferret reserato Caesaris quorum fratres caeli ter.
+            </p>
+            <img src="../assets/Ressources/Images/test.jpg">
+        </div>
+    </div>
+    <div class="bloc1">
+        <div class="bloc2">
+            <div  class="title_container">
+                <h3>
+                    Test de réflexe visuel
+                </h3></div>
+            <p>Fratres consulatu egressus confines vastabantur Constantius reges petit Valentiam ter in suo Constantius crebris dum crebris arma petit Gallorum egressus arma Arelate in limitibus Valentiam in in ter fratres suo Arelate tepore Gallorum perferret reserato Caesaris quorum fratres caeli ter.</p>
+            <img src="../assets/Ressources/Images/test.jpg">
+        </div>
+        <div class="bloc2">
+            <div  class="title_container">
+                <h3>Mesure de la fréquence cardiaque</h3></div>
+            <p>Fratres consulatu egressus confines vastabantur Constantius reges petit Valentiam ter in suo Constantius crebris dum crebris arma petit Gallorum egressus arma Arelate in limitibus Valentiam in in ter fratres suo Arelate tepore Gallorum perferret reserato Caesaris quorum fratres caeli ter.</p>
+            <img src="" alt="">
+        </div>
+
+    </div>
+
+</div>
+
+
+
+
+
+<div class="footer">
+    <p> (c) Copyright tous droits réservés Aeronic Solutions Corporation 2021 </p>
+    <div class="confidentialites">
+        <a href="confidentialite.html">Engagement de confidentialité</a>
+        <a href="Cookies.html">Utilisation de cookies</a>
+        <a href="Conditionsutilisations.html">Conditions d'utilisations</a>
+        <a href="#">Mentions légales</a>
+    </div>
+    <div class="barreimage">
+        <div class="reseaux "> <a href=" https://twitter.com/AeronicSolution "><img src="../assets/Ressources/Images/logo-twitter-noir-et-blanc-png.png" ></a></div>
+
+        <div class="reseaux"><a href="https://www.instagram.com/aeronicsolutions_/"><img src="../assets/Ressources/Images/instagram.png "></a></div>
+        <div class="reseaux"><a href="https://www.snapchat.com/add/aeronicsolution"><img src="../assets/Ressources/Images/snapchat.png"></a></div>
+
+    </div>
+</div>
+
+</body>
+</html>
