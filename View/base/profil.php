09cfd1d34052af4f6edced831aa4dee1a9a63313
charlottemrd <charlotte@milard.fr>
2021-05-05 15:36:12 +0200

    rectification fichier profil php plus de echo

--- View/base/profil.php
+++ View/base/profil.php
@@ -20,18 +20,21 @@ include 'header.php';
 include 'footer.php';
 include 'includes/database.php';
 include 'includes/requeteProfil.php';
-if ($_SESSION['utilisateurs']=='client'){
-    echo '<link rel="stylesheet" href="../assets/profils/profil.css" />';
+
+if ($_SESSION['utilisateurs']=='client'){  ?>
+    <link rel="stylesheet" href="../assets/profils/profil.css"/>
+<?php
 }
-elseif ($_SESSION['utilisateurs']=='administrateur'){
-    echo '<link rel="stylesheet" href="../assets/profils/profilAdmin.css" />';
+elseif ($_SESSION['utilisateurs']=='administrateur'){ ?>
+    <link rel="stylesheet" href="../assets/profils/profilAdmin.css" />
+<?php
 }
 else{
-    echo '<link rel="stylesheet" href="../assets/profils/profilGestionnaire.css" />';
+   ?> <link rel="stylesheet" href="../assets/profils/profilGestionnaire.css" />
+<?php
 }
 
 ?>
-
     <form action="profil.php" method="POST" class="contentconnexion">
         <div class="title_container">
             <h3>Données personnelles - <?php echo $_SESSION['utilisateurs']?></h3>
@@ -40,7 +43,7 @@ else{
             <div class="form_container">
                 <div class="information">
                     <h4>Prenom</h4>
-                    <p><?= $user['firstName']?></p>
+                    <p> <?=$user['firstName']?></p>
                 </div>
                 <div class="information">
                     <h4>E-mail</h4>
@@ -48,23 +51,23 @@ else{
                 </div>
                 <?php if ($_SESSION['utilisateurs']=='gestionnaire')
                 {
-                    echo' <div class="information">
+                    ?> <div class="information">
                     <h4>Centre médical</h4>
-                    <p>';?> <?= $user['center']?> <?php echo'</p>
-                </div>';
-                }?>
-               <?php if ($_SESSION['utilisateurs']=='client')
-               {
-                echo'<div class="information">
+                    <p> <?= $user['center']?> </p>
+                </div>
+               <?php }
+               if ($_SESSION['utilisateurs']=='client')
+               {   ?>
+                    <div class="information">
                     <h4>Date de naissance</h4>
-                    <p>';?> <?= date("d-m-Y",strtotime($user['birthDate']));?> <?php echo'</p>
+                    <p> <?= date("d-m-Y",strtotime($user['birthDate']));?> </p>
                 </div> 
                 
                 <div class="information">
                     <h4>Médecin</h4>
-                    <p>';?> <?= $user['doctor']?> <?php echo'</p>
-                </div>';
-                }?>
+                    <p> <?= $user['doctor']?> </p>
+                </div>
+              <?php  } ?>
 
             </div>
             <div class="form_container">
@@ -79,33 +82,36 @@ else{
                 </div>
 
                 <?php if ($_SESSION['utilisateurs']=='client')
-                {echo'
+                { ?>
                 <div class="information">
                     <h4>Compagnie aérienne</h4>
-                    <p>';?> <?= $user['company']?> <?php echo'</p>
+                    <p> <?= $user['company']?> </p>
                 </div>
                 
                 <div class="information">
                     <h4>Genre</h4>
-                   <p>';?> <?= $user['kind']?> <?php echo'</p>
-                </div>';
-                }?>
+                   <p> <?= $user['kind']?> </p>
+                </div>
+                <?php
+                }
+                ?>
 
             </div>
         </div>
         <div class="changement">
             <div class="bt3">
-               <input class="change" name="log" type="submit" value="Modifier Mot de Passe" >
+                <input class="change" name="log" type="submit" value="Modifier Mot de Passe" >
                 <?php
                 if(isset($_POST['log'])){
-                     header('Location: modifierMdp.php');
+                    header('Location: modifierMdp.php');
                 }
                 ?>
             </div>
             <div class="bt4">
                 <input class="change" name="logprofil" type="submit" value="Modifier Profil">
                 <?php
-                if(isset($_POST['logprofil'])){
+                if(isset($_POST['logprofil']))
+                {
                     header('Location: modifierProfil.php');
                 }
                 ?>
