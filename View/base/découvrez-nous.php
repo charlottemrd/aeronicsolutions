c0d9b33671a3b467f11de573f3f3fbcdd50ba1f8
thibsss <thibault.ragu13@gmail.com>
2021-05-07 15:57:43 +0200

    Présentation des tests terminée. Manque le footer

---
+++ View/base/découvrez-nous.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Aeronic Solutions </title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../assets/découvrez-nousStyle.css" />
+    </head>
+
+    <body>
+        <?php include 'header.php' ?>
+
+    <div class="bloc">
+        <div class="titre">
+            <h3> Découvrez-nous </h3>
+        </div>
+        <div class="contenu">
+            <h5> Qui sommes-nous ? </h5>
+            <p> Nous sommes une équipe de six jeunes ingénieurs fraichement diplômés de l'ISEP. Nous nous sommes rencontrés dans cette école et nous avons décidé de créer ensemble notre start-up : Aeronic Solutions. </p>
+            <h5>Que faisons-nous ?</h5>
+            <h5> Où travaillons-nous ? </h5>
+            <p> Notre start-up est basée dans les locaux de l'Institut Supérieur de l'Electronique de Paris (ISEP) à Issy-les-Moulineaux. Si vous voulez des renseignements pour une possible collaboration, il vous est possible de venir nous rencontrer au 2ème étage du bâtiment.</p>
+            <h5></h5>
+
+        </div>
+    </div>
+    </body>
+</html>
