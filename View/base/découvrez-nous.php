889502e7f634d8a8c0fceab52d8c9460cea7fb1e
thibsss <thibault.ragu13@gmail.com>
2021-05-11 09:14:18 +0200

    découvrez nous

--- View/base/découvrez-nous.php
+++ View/base/découvrez-nous.php
@@ -1,27 +1,82 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title> Aeronic Solutions </title>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="../assets/découvrez-nousStyle.css" />
-    </head>
+<head>
+    <title> Aeronic Solutions </title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="découvrez-nous.css" />
+</head>
 
-    <body>
-        <?php include 'header.php' ?>
+<body>
+<?php include 'header.php' ?>
 
-    <div class="bloc">
-        <div class="titre">
-            <h3> Découvrez-nous </h3>
-        </div>
-        <div class="contenu">
-            <h5> Qui sommes-nous ? </h5>
-            <p> Nous sommes une équipe de six jeunes ingénieurs fraichement diplômés de l'ISEP. Nous nous sommes rencontrés dans cette école et nous avons décidé de créer ensemble notre start-up : Aeronic Solutions. </p>
-            <h5>Que faisons-nous ?</h5>
-            <h5> Où travaillons-nous ? </h5>
-            <p> Notre start-up est basée dans les locaux de l'Institut Supérieur de l'Electronique de Paris (ISEP) à Issy-les-Moulineaux. Si vous voulez des renseignements pour une possible collaboration, il vous est possible de venir nous rencontrer au 2ème étage du bâtiment.</p>
-            <h5></h5>
-
-        </div>
+<div class="bloc">
+    <div class="titre">
+        <h3> Découvrez-nous </h3>
     </div>
-    </body>
+    <div class="contenu">
+        <h5> Qui sommes-nous ? </h5>
+        <p> Nous sommes une équipe de six jeunes ingénieurs fraichement diplômés de l'ISEP. Nous nous sommes rencontrés dans cette école et nous avons décidé de créer ensemble notre start-up : Aeronic Solutions. </br> Voici une présentation de nos membres :
+        </p>
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Charlotte.png">
+            </div>
+            <div class="presentation">
+                <p> Charlotte MILARD : </p>
+                <p> C'est Charlotte qui a eu l'idée de créer Aeronic Solution. Elle est aujourd'hui, la dirigeante du groupe. Sa devise "On peut tout finir en moins de 24h".  </p>
+            </div>
+        </div>
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Leo.png">
+            </div>
+            <div class="presentation">
+                <p>Léo KAOUA : </p>
+                <p>Léo dispose d'une très grande imagination et possède de nombreux talents notamment en dessin. Il est d'ailleurs le créateur du logo de notre start-up. Il est le web designer de l'équipe. </p>
+            </div>
+        </div>
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Chanier.png">
+            </div>
+            <div class="presentation">
+                <p> Thibault CHANIER : </p>
+                <p> Thibault est un véritable expert en programmation. Il réalise les maquettes des sites et gère les bases de données ainsi que la sécurité. </p>
+            </div>
+        </div>
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Gabriel.png">
+            </div>
+            <div class="presentation">
+                <p> Gabriel BLANCHET : </p>
+                <p> Gabriel est un grand passionné d'électronique et il aime mettre à contribution ses connaissances lors des projets. Très à l'aise en calcul, il aime résoudre les problèmes complexes. </p>
+            </div>
+        </div>
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Elena.png">
+            </div>
+            <div class="presentation">
+                <p> Elena PASCO--CASTILLOT : </p>
+                <p> Les télécommunications n'ont plus de secret pour Elena ! De même, elle aime implémenter des algorithmes et manie le langage C comme l'espagnol. </p>
+            </div>
+        </div>
+
+        <div class="conteneur">
+            <div class="image">
+                <img src="photo_Ragu.png">
+            </div>
+            <div class="presentation">
+                <p> Thibault RAGU : </p>
+                <p> Thibault gère les parties finance et marketing de Aeronic Solutions. Il participe aussi à la mise en forme des sites internet tout en appuyant Léo. </p>
+            </div>
+        </div>
+        <h5> Où travaillons-nous ? </h5>
+        <p> Notre start-up est basée dans les locaux de l'Institut Supérieur de l'Electronique de Paris (ISEP) à Issy-les-Moulineaux. Si vous voulez des renseignements pour une possible collaboration, il vous est possible de venir nous rencontrer au 2ème étage du bâtiment.</p>
+        <h5></h5>
+
+    </div>
+</div>
+</body>
 </html>
