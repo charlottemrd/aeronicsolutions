67b271d8af7d9a1607a54ec22ccdd07e8c1685ea
Elena-PC <elena.pasco-castillo@hotmail.com>
2021-05-17 14:32:18 +0200

    modification mdp oublié

--- View/base/motDePasse.php
+++ View/base/motDePasse.php
@@ -20,8 +20,13 @@ if (isset($_POST['envoi'])) {
     $from = "solutionsaeronic@gmail.com";
     $to = $_POST['mail'];
     $subject = "Nouveau mot de passe Aeronic Solutions";
-    $message = 'Vous trouverez dans ce mail, un lien de redirection qui vous permetra de redéfinir votre nouveau mot de passe afin que vous puissiez vous connecter sur le site. '. "<a href='modifierMdp.php'>MDP</a>";
+    $message1 = "Vous trouverez dans ce mail, un lien de redirection qui vous permetra de redéfinir votre nouveau mot de passe afin que vous puissiez vous connecter sur le site. 
+    Veuillez cliquer sur le lien pour le nouveau mot de passe :";
+    $message2="<a href='modifierMdp.php'>MDP</a>";
+    $message=$message1.$message2;
     $headers = "De :" . $from;
+    $headers = "MIME-Version: 1.0"."\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
     $envoi=mail($to,$subject,$message, $headers);
 
 
