ab10d38fdc84f74753820d3e052b3e918dc9769b
Thibault Chanier <49516369+Thibault-isep@users.noreply.github.com>
2021-05-25 18:11:59 +0200

    Add files via upload

---
+++ View/base/gestion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion</title>
+
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../View/assets/gestion.css" />
+
+</head>
+<body>
+
+<div class="contentconnexion">
+    <div class="title_container">
+        <h3>Maintenance du site - Aeronic Solutions</h3>
+    </div>
+    <div class="bt1">
+        <a href="gestionClientController.php">Gérer les clients</a>
+    </div>
+    <div class="bt1">
+        <a href="#">Gérer les gestionnaires</a>
+    </div>
+    <div class="bt1">
+        <a href="#">Gérer la FAQ</a>
+    </div>
+    <div class="bt1">
+        <a href="#">Gérer les capteurs</a>
+    </div>
+
+</div>
+</body>
+</html>
