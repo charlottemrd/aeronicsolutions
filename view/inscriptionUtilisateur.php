b7b36b5bf2e155b5b951438aa64359e322c616ce
Thibault Chanier <49516369+Thibault-isep@users.noreply.github.com>
2021-05-29 18:01:38 +0200

    Update inscriptionUtilisateur.php

--- view/inscriptionUtilisateur.php
+++ view/inscriptionUtilisateur.php
@@ -53,7 +53,7 @@
                        <svg width="12px" height="10px" viewbox="0 0 12 10">
                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
                        </svg></span><span>Valider les
-                       <a href="../View/base/ConditionsUtilisation.php" target="_blank">Conditions générales d'utilisations</a>
+                       <a href="../view/ConditionsUtilisation.php" target="_blank">Conditions générales d'utilisations</a>
                    </span></label>
         </div>
     </div>
