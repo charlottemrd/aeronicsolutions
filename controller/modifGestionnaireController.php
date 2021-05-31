72b84b3ace841da270c6f0b6901354d4e3e29fba
Thibault Chanier <49516369+Thibault-isep@users.noreply.github.com>
2021-05-31 22:48:21 +0200

    Update modifGestionnaireController.php

--- controller/modifGestionnaireController.php
+++ controller/modifGestionnaireController.php
@@ -14,9 +14,26 @@ if (isset($_POST['modifier'])) {
     $centre=$_POST['centre'];
     $mail=$_POST['mail'];
 
-    $sql = "UPDATE gestionnaires SET firstName = '$prenom', name='$nom', center='$centre' ,mail='$mail' WHERE icode = '$code'";
-    $reponse = $db->exec($sql);
-    header("Location: gestionGestionnaireController.php");
+    $c = $db->prepare("SELECT mail FROM administrateurs WHERE mail = :mail");
+    $c->execute(['mail' => $mail]);
+    $resultMail = $c->rowCount();
+
+    $d = $db->prepare("SELECT mail FROM clients WHERE mail = :mail");
+    $d->execute(['mail' => $mail]);
+    $resultMail += $d->rowCount();
+
+    $e = $db->prepare("SELECT mail FROM gestionnaires WHERE mail = :mail");
+    $e->execute(['mail' => $mail]);
+    $resultMail += $e->rowCount();
+
+    if ($resultMail == 0) {
+        $sql = "UPDATE gestionnaires SET firstName = '$prenom', name='$nom', center='$centre' ,mail='$mail' WHERE icode = '$code'";
+        $reponse = $db->exec($sql);
+        header("Location: gestionGestionnaireController.php");
+    }
+    else {
+        $_GET['modificationError']="L'email que vous avez entré est déjà utilisé";
+    }
 }
 else if (isset($_POST['annuler'])) {
     header("Location: gestionGestionnaireController.php");
