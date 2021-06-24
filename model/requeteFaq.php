acc21c4d32751e906aecf396ddfb4135da5c9305
Thibault Chanier <49516369+Thibault-isep@users.noreply.github.com>
2021-06-24 10:56:37 +0200

    Delete model directory

--- model/requeteFaq.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $db;
-$codeFaq = $_SESSION['modiffaq'];
-$q = $db->query("SELECT * FROM faq WHERE idfaq='$codeFaq'");
-$maQuestion = $q->fetch();
-?>
--- model/requetePilote.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $db;
-$pilote = $_SESSION['pilote'];
-$q = $db->query("SELECT * FROM clients WHERE icode='$pilote'");
-$monPilote = $q->fetch();
-?>
